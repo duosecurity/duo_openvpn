df71a18caa01f40255a64063a88c490fec45ccfc
Michael Clarke <michael.clarke@modulrfinance.com>
2021-07-31 15:32:07 +0100

    Provide the ability to use a file to configure the plugin
    The plugin currently has 3 required host and key parameters, plus 2 optional (largely undocumented) proxy parameters. Adding additional parameters in the future will be challenging as the current method of referring to positional parameters when parsing the configuration cannot handle more than one parameter - or set of related parameters - being optional.
    
    Additionally, OpenVPN will log the parameters passed to a plugin during initialisation under some logging levels which would result in the integration key, associated secret, and target host being printed to OpenVPN server logs which would then leak those values to anyone with access to the OpenVPN logs.
    
    To support multiple sets of optional parameters being allowed, an additional `--config-file=...` parameter has been added to the plugin launch arguments, with the plugin configuration then being loaded from this file if the parameter is specified. The format of the file is `name value` pairs separated by a single whitespace, with any blank lines or those prefixed by a `#` being skipped during parsing. Any other lines that do not start with a recognised configuration property result in the configuration parsing being terminated with an error.
    
    To allow fall-back for anyone specifying a legacy configuration, support for parsing positional parameters as currently supported by the plugin has been retained, although an attempt to use old and new configuration options results in the plugin failing to start.
    
    The plugin has also been updated to use the `v3` plugin structure of Open VPN which allows returning an error state from the plugin `open` phase, as well as providing the ability to report messages into the Open VPN log, thereby allowing for reporting when the plugin is not configured properly. The fallback positional parsing now logs a message in the Open VPN log warning the user they're using an out-dated configuration approach.

--- duo_openvpn.c
+++ duo_openvpn.c
@@ -16,28 +16,38 @@
 #define DUO_SCRIPT_PATH PREFIX "/duo_openvpn.pl"
 #endif
 
+static const char *PLUGIN_NAME = "DUO-OPENVPN";
+
 struct context {
 	char *ikey;
 	char *skey;
 	char *host;
 	char *proxy_host;
 	char *proxy_port;
+	plugin_log_t plugin_log;
 };
+
+static const char *
+get_variable(const char *name, const char *value, const char divider)
+{
+	int namelen = strlen(name);
+	if (!strncmp(value, name, namelen)) {
+		const char *prefix = value + namelen;
+		if (*prefix == divider) {
+			return prefix + 1;
+		}
+	}
+	return NULL;
+}
 
 static const char *
 get_env(const char *name, const char *envp[])
 {
-	int i, namelen;
-	const char *cp;
-
 	if (envp) {
-		namelen = strlen(name);
-		for (i = 0; envp[i]; ++i) {
-			if (!strncmp(envp[i], name, namelen)) {
-				cp = envp[i] + namelen;
-				if (*cp == '=') {
-					return cp + 1;
-				}
+		for (int i = 0; envp[i]; ++i) {
+			const char *result = get_variable(name, envp[i], '=');
+			if (NULL != result) {
+				return result;
 			}
 		}
 	}
@@ -126,6 +136,65 @@ auth_user_pass_verify(struct context *ctx, const char *args[], const char *envp[
 	exit(1);
 }
 
+static int
+load_configuration_file(const char *file_path, struct context *ctx)
+{
+	FILE *fp = fopen(file_path, "r");
+
+	if (!fp) {
+		ctx->plugin_log(PLOG_ERR, PLUGIN_NAME, "Could not open configuration file %s", file_path);
+		return 0;
+	}
+
+	char line[255];
+	while (fgets(line, sizeof(line), fp)) {
+		line[strcspn(line, "\r\n")] = 0;
+		if (line[0] == '\0' || line[0] == '#') {
+			continue;
+		}
+
+		if (strlen(line) > 250) {
+			ctx->plugin_log(PLOG_ERR, PLUGIN_NAME, "Configuration line longer than maximum 250 characters. Line starts with %s", line);
+			return 0;
+		}
+
+		const char *potential_ikey = get_variable("integration-key", line, ' ');
+		if (potential_ikey) {
+			ctx->ikey = strdup(potential_ikey);
+			continue;
+		}
+
+		const char *potential_skey = get_variable("secret-key", line, ' ');
+		if (potential_skey) {
+			ctx->skey = strdup(potential_skey);
+			continue;
+		}
+
+		const char *potential_host = get_variable("host", line, ' ');
+		if (potential_host) {
+			ctx->host = strdup(potential_host);
+			continue;
+		}
+
+		const char *potential_proxy_host = get_variable("proxy-host", line, ' ');
+		if (potential_proxy_host) {
+			ctx->host = strdup(potential_proxy_host);
+			continue;
+		}
+
+		const char *potential_proxy_port = get_variable("proxy-port", line, ' ');
+		if (potential_proxy_port) {
+			ctx->proxy_port = strdup(potential_proxy_port);
+			continue;
+		}
+
+		ctx->plugin_log(PLOG_ERR, PLUGIN_NAME, "Unknown configuration entry in file '%s': %s", file_path, line);
+		return 0;
+	}
+	fclose(fp);
+	return 1;
+}
+
 OPENVPN_EXPORT int
 openvpn_plugin_func_v2(openvpn_plugin_handle_t handle, const int type, const char *argv[], const char *envp[], void *per_client_context, struct openvpn_plugin_string_list **return_list)
 {
@@ -138,39 +207,79 @@ openvpn_plugin_func_v2(openvpn_plugin_handle_t handle, const int type, const cha
 	}
 }
 
-OPENVPN_EXPORT openvpn_plugin_handle_t
-openvpn_plugin_open_v2(unsigned int *type_mask, const char *argv[], const char *envp[], struct openvpn_plugin_string_list **return_list)
+OPENVPN_EXPORT int
+openvpn_plugin_open_v3(const int v3structver, const struct openvpn_plugin_args_open_in *args, struct openvpn_plugin_args_open_return *ret)
 {
 	struct context *ctx;
 
 	ctx = (struct context *) calloc(1, sizeof(struct context));
+	ctx->plugin_log = args->callbacks->plugin_log;
 
-	if (argv[1] && argv[2] && argv[3]) {
-		ctx->ikey = strdup(argv[1]);
-		ctx->skey = strdup(argv[2]);
-		ctx->host = strdup(argv[3]);
-	}
+	const char **argv = args->argv;
+	if (argv[1] && strpbrk(argv[1], "=")) {
+		const char *config_file_path = get_env("--config-file", argv);
+		if (config_file_path) {
+			if (argv[2]) {
+				ctx->plugin_log(PLOG_WARN, PLUGIN_NAME, "Plugin is configured with a config-file option and additional parameters, but parameters are not supported alongside a configuration file.");
+				return OPENVPN_PLUGIN_FUNC_ERROR;
+			}
+			if (!load_configuration_file(config_file_path, ctx)) {
+				ctx->plugin_log(PLOG_ERR, PLUGIN_NAME, "Failed to load plugin configuration from file");
+				return OPENVPN_PLUGIN_FUNC_ERROR;
+			}
+		} else {
+			ctx->plugin_log(PLOG_ERR, PLUGIN_NAME, "No 'config-file' parameter found");
+			return OPENVPN_PLUGIN_FUNC_ERROR;
+		}
+	} else {
+		if (argv[1] && argv[2] && argv[3]) {
+			ctx->plugin_log(PLOG_WARN, PLUGIN_NAME, "Plugin is configured with a legacy configuration format. Switch to using 'parameter=value' format to use any new features.");
+			ctx->ikey = strdup(argv[1]);
+			ctx->skey = strdup(argv[2]);
+			ctx->host = strdup(argv[3]);
+		}
 
-	/* Passing proxy_host even if proxy_port is not present
-	 * generates a more informative log message.
-	 */
-	if (argv[4]) {
-		ctx->proxy_host = strdup(argv[4]);
-		if (argv[5]) {
-			ctx->proxy_port = strdup(argv[5]);
+		/* Passing proxy_host even if proxy_port is not present
+		 * generates a more informative log message.
+		 */
+		if (argv[4]) {
+			ctx->proxy_host = strdup(argv[4]);
+			if (argv[5]) {
+				ctx->proxy_port = strdup(argv[5]);
+			}
+			else {
+				ctx->proxy_port = NULL;
+			}
 		}
 		else {
+			ctx->proxy_host = NULL;
 			ctx->proxy_port = NULL;
 		}
 	}
-	else {
-		ctx->proxy_host = NULL;
-		ctx->proxy_port = NULL;
+
+	int success = 1;
+	if (!ctx->host) {
+		ctx->plugin_log(PLOG_ERR, PLUGIN_NAME, "Host is not specified in plugin configuration");
+		success = 0;
+	}
+	if (!ctx->ikey) {
+		ctx->plugin_log(PLOG_ERR, PLUGIN_NAME, "Integration Key is not specified in plugin configuration");
+		success = 0;
+	}
+	if (!ctx->skey) {
+		ctx->plugin_log(PLOG_ERR, PLUGIN_NAME, "Secret Key is not specified in plugin configuration");
+		success = 0;
 	}
 
-	*type_mask = OPENVPN_PLUGIN_MASK(OPENVPN_PLUGIN_AUTH_USER_PASS_VERIFY);
+	if (success != 1) {
+		return OPENVPN_PLUGIN_FUNC_ERROR;
+	}
 
-	return (openvpn_plugin_handle_t) ctx;
+	ctx->plugin_log(PLOG_NOTE, PLUGIN_NAME, "Plugin initialised with host '%s' and integration key '%s'", ctx->host, ctx->ikey);
+
+	ret->type_mask = OPENVPN_PLUGIN_MASK(OPENVPN_PLUGIN_AUTH_USER_PASS_VERIFY);
+	ret->handle = (openvpn_plugin_handle_t) ctx;
+	return OPENVPN_PLUGIN_FUNC_SUCCESS;
 }
 
 OPENVPN_EXPORT void
